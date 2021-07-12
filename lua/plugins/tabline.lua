856737e59853a916f93778c17206dcc1359128cd
kingavatar <saikiranreddy06@gmail.com>
2021-07-13 04:43:26 +0530

    Updated Bufferline plugin Config

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -1,9 +1,12 @@
 require "bufferline".setup {
     options = {
-        offsets = {{filetype = "NvimTree", text = "", padding = 1}},
+        offsets = {
+          {filetype = "NvimTree", text = "File Explorer", highlight = "Directory", padding = 1},
+          {filetype = "Outline", text = "Symbols Outline", highlight = "Title", padding = 1}
+        },
         buffer_close_icon = "",
         modified_icon = "",
-        close_icon = "",
+        close_icon = "", --  --
         left_trunc_marker = "",
         right_trunc_marker = "",
         max_name_length = 14,
@@ -14,18 +17,51 @@ require "bufferline".setup {
         view = "multiwindow",
         show_buffer_close_icons = true,
         separator_style = "thin",
-        mappings = "true"
-    }
+        mappings = "true",
+        diagnostics = "nvim_lsp",
+        ---@diagnostic disable-next-line: unused-local
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            if context.buffer:current() then
+                return ''
+            end
+
+            return ' '
+        end
+    },
+      highlights = {
+            diagnostic = {
+                guifg = "#db4b4b",
+            }
+      }
+
 }
 
-local opt = {silent = true}
-local map = vim.api.nvim_set_keymap
+local opt = {}
+local map = require("mappings").map
+
 vim.g.mapleader = ","
 
+function BufDelete ()
+	 if vim.fn.len(vim.fn.filter(vim.fn.range(1, vim.fn.bufnr('$')), '!empty(bufname(v:val)) && buflisted(v:val)')) == 1 then
+	   vim.api.nvim_command("q")
+	 else
+	   vim.api.nvim_command("Bdelete")
+	 end
+end
 -- MAPPINGS
 map("n", "<S-t>", [[<Cmd>tabnew<CR>]], opt) -- new tab
-map("n", "<S-x>", [[<Cmd>Bdelete<CR>]], opt) -- close tab
+-- map("n", "<S-x>", [[<Cmd>BufDelete()<CR>]], opt) -- close tab
+
+local wk = require('which-key')
+wk.register({
+    ["<S-x>"] = { BufDelete , "Close Tab" }
+},  {noremap = true, mode="n"})
 
 -- move between tabs
-map("n", "<TAB>", [[<Cmd>BufferLineCycleNext<CR>]], opt)
-map("n", "<S-TAB>", [[<Cmd>BufferLineCyclePrev<CR>]], opt)
+map("n", "<TAB>", [[<Cmd>BufferLineCycleNext<CR>]], {noremap = true})
+map("n", "<S-TAB>", [[<Cmd>BufferLineCyclePrev<CR>]], {noremap = true})
+
+-- move the tab
+map("n", "[b", [[<Cmd>BufferLineMoveNext<CR>]], {noremap = true})
+map("n", "]b", [[<Cmd>BufferLineMovePrev<CR>]], {noremap = true})
+
